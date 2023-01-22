5fa6822f32aec185623b2f372d93361d7b1111da
Ishan Karmakar <ishan.karmakar24@gmail.com>
2023-01-22 05:26:51 -0600

    Decompressor completed

--- src/decompressor.cpp
+++ src/decompressor.cpp
@@ -30,7 +30,7 @@ Node* read_tree(FILE* file) {
     return root;
 }
 
-unsigned char get_bit(const char* const buffer, unsigned char& byte_count, unsigned char& bit_count) {
+unsigned char get_bit(const char* const buffer, unsigned int& byte_count, unsigned char& bit_count) {
     char byte = buffer[byte_count];
     unsigned char bit = (byte >> bit_count) & 1;
     (bit_count)++;
@@ -41,8 +41,9 @@ unsigned char get_bit(const char* const buffer, unsigned char& byte_count, unsig
     return bit;
 }
 
-void get_chars(const char* const buffer, const unsigned int bufsize, Node* cur_node, Node* const root) {
-    unsigned char byte_count = 0, bit_count = 0;
+void get_chars(const char* const buffer, const unsigned int bufsize, char* const outbuf, Node* cur_node, Node* const root, FILE* outfile) {
+    unsigned int byte_count = 0, out_byte_count = 0;
+    unsigned char bit_count = 0;
     for (int i = 0; i < bufsize * BITS_PER_BYTE; i++) {
         unsigned char bit = get_bit(buffer, byte_count, bit_count);
         if (bit)
@@ -50,28 +51,36 @@ void get_chars(const char* const buffer, const unsigned int bufsize, Node* cur_n
         else
             cur_node = cur_node->left;
         if (cur_node->leaf) {
-            if (!cur_node->pseudo)
-                std::cout << cur_node->chr;
+            if (!cur_node->pseudo) {
+                outbuf[out_byte_count] = cur_node->chr;
+                out_byte_count++;
+                if (out_byte_count == BUFFER_SIZE)
+                    fwrite(outbuf, sizeof(char), BUFFER_SIZE, outfile);
+            }
             cur_node = root;
         }
     }
+    if (out_byte_count)
+        fwrite(outbuf, sizeof(char), out_byte_count, outfile);
     std::cout << std::endl;
 }
 
-void read_file_contents(FILE* file, const unsigned int num_bytes, Node* const root) {
+void read_file_contents(FILE* file, FILE* outfile, const unsigned int num_bytes, Node* const root) {
     Node cur_node = *root;
     char* buffer = new char[BUFFER_SIZE];
+    char* outbuf = new char[BUFFER_SIZE];
     unsigned int full_chunks = num_bytes / BUFFER_SIZE;
     unsigned int leftover_bytes = num_bytes % BUFFER_SIZE;
     for (int i = 0; i < full_chunks; i++) {
         fread(buffer, sizeof(char), BUFFER_SIZE, file);
-        get_chars(buffer, BUFFER_SIZE, &cur_node, root);
+        get_chars(buffer, BUFFER_SIZE, outbuf, &cur_node, root, outfile);
     }
     if (leftover_bytes) {
         fread(buffer, sizeof(char), leftover_bytes, file);
-        get_chars(buffer, leftover_bytes, &cur_node, root);
+        get_chars(buffer, leftover_bytes, outbuf, &cur_node, root, outfile);
     }
     delete[] buffer;
+    delete[] outbuf;
 }
 
 void printBT(const std::string& prefix, const Node* node, bool isLeft)
@@ -103,9 +112,12 @@ void printBT(const Node* node)
 int main() {
     std::string filename = "out.bin";
     FILE* file = fopen(filename.c_str(), "rb");
+    std::string out_filename = "decompressed.txt";
+    FILE* outfile = fopen(out_filename.c_str(), "wb");
     Node* root = read_tree(file);
     unsigned int num_bytes;
     fread(&num_bytes, sizeof(num_bytes), 1, file);
-    read_file_contents(file, num_bytes, root);
+    read_file_contents(file, outfile, num_bytes, root);
     fclose(file);
+    fclose(outfile);
 }
