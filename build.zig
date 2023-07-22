8c83f5d1723a028a4d9cf4fc0f2755c90d6dca76
Ishan Karmakar <ishan.karmakar24@gmail.com>
2023-07-22 18:39:08 -0500

    Modified build system

--- build.zig
+++ build.zig
@@ -10,6 +10,9 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize
     });
+    b.installArtifact(compressor);
+    var compressor_cmd = b.addRunArtifact(compressor);
+    compressor_cmd.step.dependOn(b.getInstallStep());
 
     var decompressor = b.addExecutable(.{
         .name = "decompressor",
@@ -17,19 +20,16 @@ pub fn build(b: *Build) void {
         .target = target,
         .optimize = optimize
     });
+    b.installArtifact(decompressor);
+    var decompressor_cmd = b.addRunArtifact(decompressor);
+    decompressor_cmd.step.dependOn(b.getInstallStep());
 
-    var compressor_cmd = (b.addRunArtifact(compressor));
-    var decompressor_cmd = (b.addRunArtifact(decompressor));
-    if (b.args != null) {
-        compressor_cmd.addArgs(b.args.?);
-        decompressor_cmd.addArgs(b.args.?);
+    if (b.args) |args| {
+        compressor_cmd.addArgs(args);
+        decompressor_cmd.addArgs(args);
     }
     const compressor_run = b.step("run-compressor", "Run compressor");
     const decompressor_run = b.step("run-decompressor", "Run decompressor");
     compressor_run.dependOn(&compressor_cmd.step);
     decompressor_run.dependOn(&decompressor_cmd.step);
-    b.installArtifact(compressor);
-    b.installArtifact(decompressor);
-    b.default_step.dependOn(&compressor.step);
-    b.default_step.dependOn(&decompressor.step);
 }
